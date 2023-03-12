1a41d36a44d8d814cacb288213e7963c700dceab
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-03-12 10:14:13 +0100

    started validation of signup

---
+++ src/js/signup-farmers.js
@@ -0,0 +1,18 @@
+const validation = () => {
+   
+    let error = 0;
+    let fullName = document.getElementById("fullname");
+    let email = document.getElementById("email");
+    let dor = document.getElementById("dor");
+    let dob = document.getElementById("dob");
+    let nin = document.getElementById("nin");
+    let unique = document.getElementById('unique');
+    let gmale = document.getElementById("gmale");
+    let gfemale = document.getElementById("gfemale");
+    let ward = document.getElementById("ward");
+    let password = document.getElementById("password");
+    let repassword = document.getElementById("repassword");
+
+
+    
+}
