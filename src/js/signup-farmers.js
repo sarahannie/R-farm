4ad01830f835f7a6ea1f0152feae75c5b82f1c7c
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-03-15 15:07:40 +0100

    signup validation done

--- src/js/signup-farmers.js
+++ src/js/signup-farmers.js
@@ -42,7 +42,7 @@ const validation = (event) => {
     let ninErr = document.getElementById("ninErr");
     let uniqueErr = document.getElementById('uniqueErr');
     let gfemaleErr = document.getElementById("gfemaleErr");
-    let phonenoErr = document.getElementById("phonenumErr")
+    
     
     let passwordError = document.getElementById('passwordErr');
     let repasswordError = document.getElementById('repasswordErr');
@@ -51,27 +51,27 @@ const validation = (event) => {
 if (isValid) {
     if (fullName.value == "") {
       fullName.style.border = "1px solid red";
-      fnameErr.innerHTML = "Please input your FullName";
+      fnameErr.textContent = "Please input your FullName";
       fnameErr.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
       isValid = false;
     } else if (!/^[a-zA-Z\s]+$/.test(fullName.value)) {
       fullName.style.border = "1px solid red";
-      fnameErr.innerHTML = "Please input vaild name ";
+      fnameErr.textContent = "Please input vaild name ";
       fnameErr.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
       isValid = false;
     } else if (fullName.value.length < 5) {
       fullName.style.border = "1px solid red";
-      fnameErr.innerHTML = "Please the full name must be atleast 5 letters";
+      fnameErr.textContent = "Please the full name must be atleast 5 letters";
       fnameErr.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
       isValid = false;
     } else if (fullName.value.length > 50) {
       fullName.style.border = "1px solid red";
-      fnameErr.innerHTML = "Please the full name should not exceed 50 letters";
+      fnameErr.textContent = "Please the full name should not exceed 50 letters";
       fnameErr.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
       isValid = false;
     } else {
       fullName.style.border = "1px solid green";
-      fullName.innerHTML = "";
+      fnameErr.textContent = "";
     }
 } 
 
@@ -105,25 +105,37 @@ if (isValid) {
 
     //validation of date of birth
     if (isValid) {  
-        let dobValue = dob.value;
-        let dobDate = new Date(dobValue);
-        //calculating user age
-
-        let ageDiff = Date.now() - dobDate.getTime();
-        let ageDate = new Date(ageDiff);
-        let age = Math.abs(ageDate.getUTCFullYear() - 1970)
-
-        if( age < 10 ){
-            event.preventDefault();
-            dob.style.border = "1px solid red";
-            dobErr.textContent = "User must be more than ten year old ";
-            dobErr.style="color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;"
-            isValid = false;
-        }else{
-            dob.style.border = "1px solid green";
-            dobErr.textContent = " "; 
-        }
-    } 
+      let dobValue = dob.value;
+      let dobDate = new Date(dobValue);
+  
+      //calculating user age
+      let ageDiff = Date.now() - dobDate.getTime();
+      let ageDate = new Date(ageDiff);
+      let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+  
+      if (age < 10) {
+          event.preventDefault();
+          dob.style.border = "1px solid red";
+          dobErr.textContent = "User must be at least 10 years old.";
+          dobErr.style.color = "red";
+          dobErr.style.fontSize = "11px";
+          dobErr.style.fontFamily = "Arial, Helvetica, sans-serif";
+          isValid = false;
+      } else if (dobValue == ""){
+        event.preventDefault();
+          dob.style.border = "1px solid red";
+          dobErr.textContent = "User must  add date of birth.";
+          dobErr.style.color = "red";
+          dobErr.style.fontSize = "11px";
+          dobErr.style.fontFamily = "Arial, Helvetica, sans-serif";
+          isValid = false;
+      }
+      else {
+          dob.style.border = "1px solid green";
+          dobErr.textContent = ""; 
+      }
+  }
+   
     
     //validation of nin
     if (isValid) {
@@ -144,9 +156,9 @@ if (isValid) {
         nin.style.border = " 1px solid green" ;
         ninErr.textContent=""
     }
-
+  }
     //validation for unique
-
+    if (isValid) {
     const foregex = /^FO-([0-9]{3})+$/
     if (unique.value == "") {
       unique.style.border = "1px solid red"
@@ -163,132 +175,151 @@ if (isValid) {
         uniqueErr.textContent = "";
       }
     }
+  
 
     // validation of gender start
     if (isValid) {
-    if(!(gmale.checked && gfemale.checked)){
-       gfemaleErr.textContent = "Please select gender";
-       gfemaleErr.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
-       isValid = false; 
-    }else{
-        gfemaleErr.textContent = "";
-    }
-}
+      if (!gmale.checked && !gfemale.checked) {
+          gmale.style.border = "1px solid red";
+          gfemale.style.border = "1px solid red";
+          gfemaleErr.textContent = "Please select your gender";
+          gfemaleErr.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+          isValid = false;
+      } else {
+          gmale.style.border = "none";
+          gfemale.style.border = "none";
+          gfemaleErr.textContent = "";
+      }
+  }
 
 
     // validation for phone number
-if (isValid) {
-    // Get the phone number input element
-const phoneNumberInput = document.getElementById('phoneInput');
+    if (isValid) {
+      // Get the phone number input element
+      const phoneNumberInput = document.getElementById('phoneInput');
+    
+      // Get the selected country code from the dropdown
+      const countryCodeDropdown = document.getElementById('country-code');
+      const selectedCountryCode = countryCodeDropdown.options[countryCodeDropdown.selectedIndex].text;
+    
+      // Define a regular expression pattern for the selected country code
+      let phonePattern;
+      switch (selectedCountryCode) {
+        case '+256':
+          phonePattern = /^07\d{8}$/; // Uganda phone number format: 07XXXXXXXX
+          break;
+        case '+254':
+          phonePattern = /^07\d{8}$/; // Kenya phone number format: 07XXXXXXXX
+          break;
+        case '+255':
+          phonePattern = /^07\d{8}$/; // Tanzania phone number format: 07XXXXXXXX
+          break;
+        case '+234':
+          phonePattern = /^0\d{10}$/; // Nigeria phone number format: 0XXXXXXXXXX
+          break;
+        default:
+          phonePattern = /^\d+$/; // Default pattern for other country codes
+          break;
+      }
+    
+      // Get the error message element
+      const phonenoErr = document.getElementById('phonenumErr');
+    
+      // Add an event listener to the phone number input to validate its value
+      phoneNumberInput.addEventListener('blur', function() {
+        const phoneValue = this.value;
+        if (!phoneValue.match(phonePattern)) {
+          phoneNumberInput.style.border = "1px solid red";
+          phonenoErr.textContent = "Please enter a valid phone number";
+          phonenoErr.style.color = "red";
+          isValid = false;
+        } else if (phoneValue === "") {
+          phoneNumberInput.style.border = "1px solid red";
+          phonenoErr.textContent = "Please enter a phone number";
+          phonenoErr.style.color = "red";
+          isValid = false;
+        } else {
+          phoneNumberInput.style.border = "1px solid green";
+          phonenoErr.textContent = "";
+          
+        }
+      });
+    }
+    
 
-// Get the selected country code from the dropdown
-const countryCodeDropdown = document.getElementById('country-code');
-const selectedCountryCode = countryCodeDropdown.options[countryCodeDropdown.selectedIndex].text;
 
-// Define a regular expression pattern for the selected country code
-let phonePattern;
-switch (selectedCountryCode) {
-  case '+256':
-    phonePattern = /^07\d{8}$/; // Uganda phone number format: 07XXXXXXXX
-    break;
-  case '+254':
-    phonePattern = /^07\d{8}$/; // Kenya phone number format: 07XXXXXXXX
-    break;
-  case '+255':
-    phonePattern = /^07\d{8}$/; // Tanzania phone number format: 07XXXXXXXX
-    break;
-  case '+234':
-    phonePattern = /^0\d{10}$/; // Nigeria phone number format: 0XXXXXXXXXX
-    break;
-  default:
-    phonePattern = /^\d+$/; // Default pattern for other country codes
-    break;
-}
 
-// Add an event listener to the phone number input to validate its value
-phoneNumberInput.addEventListener('blur', function() {
-  const phoneValue = this.value;
-  if (!phoneValue.match(phonePattern)) {
-    phoneNumberInput.style.border = "1px solid red";
-    phonenoErr.textContent ="Please enter valid number";
-    phonenoErr.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
-    isValid = false;
-  } 
-  else if (phoneValue == "") {
-    phoneNumberInput.style.border = "1px solid red";
-    phonenoErr.textContent ="Please enter phone number";
-    phonenoErr.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
-    isValid = false;
-  } 
-  else {
-    phoneNumberInput.style.border = "1px solid green";
-    phoneNumberInput.focus();
-  }
-});
-}
 
 if (isValid) {
 
 // Activities validation
-const activitiesDropdown = document.getElementById('activities');
+let activities = document.getElementById("activities");
+let activitiesErr = document.getElementById("activitiesErr");
 
-activitiesDropdown.addEventListener('change', function() {
-  const selectedActivity = activitiesDropdown.value;
-  
-  if (selectedActivity === '') {
-    // No activity selected, display error message
-    activitiesDropdown.classList.add('is-invalid');
+
+if (activities.value == "Activites Undertaken") {
+    activities.style.border = "1px solid red";
+    activitiesErr.textContent = "Please select an activity";
+    activitiesErr.style="color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;"
     isValid = false;
-  } else {
-    // Valid activity selected, remove error message (if any)
-    activitiesDropdown.classList.remove('is-invalid');
-  }
-});
+} else {
+    activities.style.border = "1px solid green";
+    activitiesErr.textContent = " "; 
 }
+}
+
+
 
 if (isValid) {
-
-// validation for password
-function validatePassword() {
-    const password = passwordInput.value;
-    const repassword = repasswordInput.value;
-    
-    if (password === '' || password.length < 8) {
-      // Password is too short or empty
-      passwordInput.classList.add('is-invalid');
-      passwordError.textContent = 'Password must be at least 8 characters long';
-      passwordError.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+  const passwordregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  if (passwordInput.value == "") {
+      passwordInput.style.border = "1px solid red";
+      passwordError.textContent = "Password is required";
+      passwordError.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
       isValid = false;
-    } else {
-      // Password is valid, remove error message (if any)
-      passwordInput.classList.remove('is-invalid');
-      passwordError.textContent = '';
-    }
-    
-    if (repassword === '' || repassword !== password) {
-      // Re-entered password doesn't match or is empty
-      repasswordInput.classList.add('is-invalid');
-      repasswordError.textContent = 'Passwords do not match';
-      repasswordError.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+  } else if (!passwordInput.value.match(passwordregex)) {
+      passwordInput.style.border = "1px solid red";
+      passwordError.textContent = "Password should be minimum eight characters, at least one uppercase letter, one lowercase letter and one number";
+      passwordError.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
       isValid = false;
-    } else {
-      // Re-entered password matches, remove error message (if any)
-      repasswordInput.classList.remove('is-invalid');
-      repasswordError.textContent = '';
-    }
+  } else {
+      passwordInput.style.border = "none";
+      passwordError.textContent = "";
   }
-  
-  passwordInput.addEventListener('input', validatePassword);
-  repasswordInput.addEventListener('input', validatePassword);
-
-
 }
 
+// validation for re-entered password
+if (isValid) {
+  if (repasswordInput.value == "") {
+      repasswordInput.style.border = "1px solid red";
+      repasswordError.textContent = "Please re-enter your password";
+      repasswordError.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+      isValid = false;
+  } else if (repasswordInput.value !== passwordInput.value) {
+      repasswordInput.style.border = "1px solid red";
+      repasswordError.textContent = "Passwords do not match";
+      repasswordError.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+      isValid = false;
+  } else {
+      repasswordInput.style.border = "none";
+      repasswordError.textContent = "";
+  }
+}
+
+
+
     // if all validation passed, submit the form
-    if (isValid) {
-        event.preventDefault();
-        form.submit();
-        }
+    // if (!isValid) {
+    //     event.preventDefault();
+        
+    //     }
+
+    //    if (isValid) {
+        
+    //       form.submit();
+    //       }
+
+    return isValid;
      }
    
 
